42af5e659096348bb9b581a3062926fbb5c51fa6
Owen Henley <owenhenley@me.com>
2023-03-22 14:28:54 +0000

    create sections for editing

--- Today/Today/ViewController/ReminderViewController.swift
+++ Today/Today/ViewController/ReminderViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ReminderViewController: UICollectionViewController {
-    private typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
-    private typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Row>
-
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, Row>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Row>
+    
     var reminder: Reminder
     private var dataSource: DataSource!
-
+    
     init(reminder: Reminder) {
         self.reminder = reminder
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
@@ -21,30 +21,30 @@ class ReminderViewController: UICollectionViewController {
         let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
         super.init(collectionViewLayout: listLayout)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("Always initialize ReminderViewController using init(reminder:)")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-
+        
         if #available(iOS 16, *) {
             navigationItem.style = .navigator
         }
         navigationItem.title = NSLocalizedString("Reminder", comment: "Reminder view controller title")
-
+        
         updateSnapshot()
     }
-
+    
     private func setup() {
         let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
         dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Row) in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
     }
-
+    
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = text(for: row)
@@ -53,7 +53,7 @@ class ReminderViewController: UICollectionViewController {
         cell.contentConfiguration = contentConfiguration
         cell.tintColor = .todayPrimaryTint
     }
-
+    
     func text(for row: Row) -> String? {
         switch row {
         case .date:
@@ -66,11 +66,19 @@ class ReminderViewController: UICollectionViewController {
             return reminder.title
         }
     }
-
+    
     private func updateSnapshot() {
         var snapshot = Snapshot()
-        snapshot.appendSections([0])
-        snapshot.appendItems([Row.title, Row.date, Row.time, Row.notes], toSection: 0)
+        snapshot.appendSections([.view])
+        snapshot.appendItems([Row.title, Row.date, Row.time, Row.notes], toSection: .view)
         dataSource.apply(snapshot)
+    }
+    
+    private func section(for indexPath: IndexPath) -> Section {
+        let sectionNumber = isEditing ? indexPath.section + 1 : indexPath.section
+        guard let section = Section(rawValue: sectionNumber) else {
+            fatalError("Unable to find matching section")
+        }
+        return section
     }
 }
